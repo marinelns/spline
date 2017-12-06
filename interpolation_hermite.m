1bb7fc4b703954deea39a8949ed1a6de6042d36a
Noé Pion <noe.pion@grenoble-inp.org>
2017-12-06 12:53:19 +0100

    je suis bête

---
+++ interpolation_hermite.m
@@ -0,0 +1,12 @@
+function [points_interpol] = interpolation_hermite(bk,resolution)
+    %calcule les points de l'interpolation d'hermite sur un morceau
+    % defini par ses pts de control
+    points_interpol = [0;0];
+    for j = 1:resolution
+        s = 0;
+        for i = 1:4
+            s = s + bk(:,i)*bernstein(i-1,3,(j-1)/resolution);
+        end
+        points_interpol(:,j) = s;
+    end
+end
